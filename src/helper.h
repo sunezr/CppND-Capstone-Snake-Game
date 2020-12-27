e92ad8a4e0e31d3b6866a37309d054b121903591
frank <franksunus@gmail.com>
2020-12-27 20:21:34 +0800

    Update helper.h
    Add assertion

--- src/helper.h
+++ src/helper.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <cassert>
 
 template<typename T>
 T readParameter(std::string parameterName) {
@@ -12,17 +13,20 @@ T readParameter(std::string parameterName) {
     std::string line;
     std::string key;
     T value;
+    bool flag{false};
     std::ifstream filestream(filePath);
     if (filestream.is_open()) {
         while (std::getline(filestream, line)) {
             std::istringstream linestream(line);
             if (linestream >> key >> value) {
                 if (key == parameterName + ':') {
+                    flag = true;
                     break;
                 }
             }
         }
     }
+    assert(flag); // make sure key is found
     return value;
 }
 
